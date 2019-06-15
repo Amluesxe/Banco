88192edb8fd919c0b3561ed7855d1516851f0e5a
JonasUnal <51845041+JonasUnal@users.noreply.github.com>
2019-06-15 12:12:40 -0500

    Update Persona.java

--- src/data/Persona.java
+++ src/data/Persona.java
@@ -4,32 +4,67 @@ public class Persona{
 private String nombre;
 private String apellido;
 private String documento;
+private String edad;
+private String sexo;
+private Dia fechaDeNacimiento;
 
-public Persona(String nombre, String apellido,String documento){
-this.nombre=nombre;
-this.apellido=apellido;
-this.documento=documento;
-}
+    public Persona(String nombre, String apellido, String documento, String edad, String sexo, Dia fechaDeNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
 
-public String getNombre(){
-return nombre;
-}
-public void setNombre(String nombre){
-this.nombre=nombre;
-}
+    public String getNombre() {
+        return nombre;
+    }
 
-public String getApellido(){
-return apellido;
-}
-public void setApellido(String apellido){
-this.apellido=apellido;
-}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-public String getDocumento(){
-return documento;
-}
-public void setDocumento(String documento){
-this.documento=documento;
-}
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Dia getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(Dia fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+        
 public Persona(){}
 }
